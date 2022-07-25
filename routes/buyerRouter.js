b3e6f7681247c0a41e1436d1d0b3580def508500
Evgenia-bit <ev.mo4alina@yandex.ru>
2022-07-25 14:21:55 +0300

    improved formatting

--- routes/buyerRouter.js
+++ routes/buyerRouter.js
@@ -1,49 +1,58 @@
 const Router = require('express')
-const router = new Router
 const db = require('../db')
 
-router.post('/create', (req, res) => {
-    (async () => {
-        try {
-            const { surname, name, patronymic, series, number, whoIssued, whenIssued, place, phone } = req.body
-            await db.query(`INSERT INTO Покупатель ( Фамилия, Имя, Отчество, СерияПаспорта, НомерПаспорта, ВремяВыдачиПаспорта, КемВыданПаспорт, МестоЖительства, НомерТелефона )
-            VALUES ('${surname}',  '${name}', '${patronymic}', '${series}', '${number}', '${whenIssued}', '${whoIssued}',  '${place}', '${phone}' ) `);
-                return res.json({ msg: 'Покупатель успешно создан!', status: 'OK' })
-        } catch (e) {
-            console.log(e)
-            return res.json({ msg: 'Произошла ошибка!', status: 'error' })
-        }
-    })()
+const router = new Router
+
+router.post('/create', async (req, res) => {
+    try {
+        const {surname, name, patronymic, series, number, whoIssued, whenIssued, place, phone} = req.body
+
+        await db.query(`INSERT INTO Покупатель ( Фамилия, Имя, Отчество, СерияПаспорта, НомерПаспорта, ВремяВыдачиПаспорта, КемВыданПаспорт, МестоЖительства, НомерТелефона )
+            VALUES ('${surname}',  '${name}', '${patronymic}', '${series}', '${number}', '${whenIssued}', '${whoIssued}',  '${place}', '${phone}' ) `)
+
+        return res.json({msg: 'Покупатель успешно создан!', status: 'OK'})
+    } catch (e) {
+        return res.json({msg: 'Произошла ошибка!', status: 'error'})
+    }
 })
-router.delete('/delete', (req, res) => {
-    (async () => {
-        try {
-            await db.query(`DELETE FROM Покупатель WHERE Фамилия = '${req.body[0]}' AND Имя = '${req.body[1]}' AND  Отчество = '${req.body[2]}' AND  СерияПаспорта = '${req.body[3]}' AND НомерПаспорта = '${req.body[4]}'`) 
-            return res.json( {msg : 'Покупатель успешно удалён!', status: 'OK'})
-        } catch (e) {
-            return res.json({ msg: 'Произошла ошибка!', status: 'error' })
-        }
-    })()
+
+router.delete('/delete', async (req, res) => {
+    try {
+        await db.query(`DELETE FROM Покупатель WHERE Фамилия = '${req.body[0]}' AND Имя = '${req.body[1]}' AND  Отчество = '${req.body[2]}' AND  СерияПаспорта = '${req.body[3]}' AND НомерПаспорта = '${req.body[4]}'`)
+
+        return res.json({msg: 'Покупатель успешно удалён!', status: 'OK'})
+    } catch (e) {
+        return res.json({msg: 'Произошла ошибка!', status: 'error'})
+    }
 })
-router.get('/all', (req, res) => {
-    (async () => {
-        try {
-            const films = await db.query(`SELECT КодПокупателя AS "Код покупателя", Фамилия, Имя, Отчество, СерияПаспорта AS "Серия паспорта", НомерПаспорта AS "Номер паспорта", ВремяВыдачиПаспорта AS "Время выдачи паспорта", КемВыданПаспорт AS "Кем выдан паспорт", МестоЖительства AS "Место жительства", НомерТелефона AS "Номер телефона" FROM Покупатель`)
-            let elements = {}
-            let codes = {}
-            let fullnames = {}
-            films.rows.forEach((elem, i) => {
-                codes[i] = elem['Код покупателя']
-                elem['Время выдачи паспорта'] = elem['Время выдачи паспорта'].toLocaleDateString()
-                elements[i] = elem;
-                fullnames[i] = elem['Фамилия'] +  ' ' + elem['Имя'][0] + '. ' + elem['Отчество'][0] + '. ';
-            });
-            return res.json( {msg : 'Покупатели успешно получены!', status: 'OK', title: 'Все покупатели', elements, codes,  fullnames })
-        } catch (e) {
-            console.log(e)
-            return res.json({ msg: 'Произошла ошибка!', status: 'error' })
-        }
-    })()
+
+router.get('/all', async (req, res) => {
+    try {
+        const films = await db.query(`SELECT КодПокупателя AS "Код покупателя", Фамилия, Имя, Отчество, СерияПаспорта AS "Серия паспорта", НомерПаспорта AS "Номер паспорта", ВремяВыдачиПаспорта AS "Время выдачи паспорта", КемВыданПаспорт AS "Кем выдан паспорт", МестоЖительства AS "Место жительства", НомерТелефона AS "Номер телефона" FROM Покупатель`)
+
+        let elements = {}
+        let codes = {}
+        let fullnames = {}
+
+        films.rows.forEach((elem, i) => {
+            codes[i] = elem['Код покупателя']
+            elem['Время выдачи паспорта'] = elem['Время выдачи паспорта'].toLocaleDateString()
+            elements[i] = elem;
+            fullnames[i] = elem['Фамилия'] + ' ' + elem['Имя'][0] + '. ' + elem['Отчество'][0] + '. ';
+        });
+
+        return res.json({
+            msg: 'Покупатели успешно получены!',
+            status: 'OK',
+            title: 'Все покупатели',
+            elements,
+            codes,
+            fullnames
+        })
+    } catch (e) {
+        console.log(e)
+        return res.json({msg: 'Произошла ошибка!', status: 'error'})
+    }
 })
 
 module.exports = router
